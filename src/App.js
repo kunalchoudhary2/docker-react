e21c0a320da1f78de46f1497b465fd9c9dc4f879
root <root@ip-172-31-24-119.us-west-1.compute.internal>
2019-08-11 19:15:00 +0000

    changed src/app.js file message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There ..again ..Edit <code>src/App.js</code> and save to reload.
+         i was changed on the feature branch.  Hi There ..again ..Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
